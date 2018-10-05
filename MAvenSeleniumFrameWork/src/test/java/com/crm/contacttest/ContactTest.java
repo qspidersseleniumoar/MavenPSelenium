060b4c6bdfc23efc7be2458f0ac3f2e038291136
Administrator <Administrator@studentapp>
2018-10-05 17:03:38 +0530

    maven selenium test

---
+++ MAvenSeleniumFrameWork/src/test/java/com/crm/contacttest/ContactTest.java
@@ -0,0 +1,30 @@
+package com.crm.contacttest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void createConatctTest() {
+		System.out.println("execute createConatctTest");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://gmail.com");
+		driver.close();
+	}
+	
+	@Test
+	public void modifyConatctTest() {
+		System.out.println("execute modifyConatctTest");
+
+	}
+	
+	@Test
+	public void deleteConatctTest() {
+		System.out.println("execute deleteConatctTest");
+
+	}
+
+
+}
