71b273e887fbbe352e8deb1ac0820698b72bb67c
Administrator <Administrator@studentapp>
2018-10-05 17:45:21 +0530

    add

---
+++ MAvenSeleniumFrameWork/src/test/java/com/crm/contacttest/OrgTest.java
@@ -0,0 +1,18 @@
+package com.crm.contacttest;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	
+	@Test
+	public void cerateOrgTest() {
+		System.out.println("test 3");
+	}
+
+	@Test
+	public void modifyORgTest() {
+		System.out.println("test 4");
+	}
+
+}
